95aff644ef58ab9b4e4c2efe12576760feafee0e
wambuimweru@gmail.com <wambuimweru@gmail.com>
2021-07-10 13:42:54 -0700

    I digit

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,4 +1,3 @@
-julien@ubuntu:~/0x04$ cat 1-main.c 
 #include "holberton.h"
 #include <stdio.h>
 
@@ -17,8 +16,3 @@ int main(void)
     printf("%c: %d\n", c, _isdigit(c));
     return (0);
 }
-julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-isdigit.c -o 1-isdigit
-julien@ubuntu:~/0x04$ ./1-isdigit 
-0: 1
-a: 0
-julien@ubuntu:~/0x04$
