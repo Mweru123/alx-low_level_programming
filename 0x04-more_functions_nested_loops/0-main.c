6feb20cfff15d7c21acb66fd1ffaabde74c83977
wambuimweru@gmail.com <wambuimweru@gmail.com>
2021-07-10 13:35:14 -0700

    initial commit

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,4 +1,3 @@
-julien@ubuntu:~/0x04$ cat 0-main.c
 #include "holberton.h"
 #include <stdio.h>
 
@@ -17,8 +16,3 @@ int main(void)
     printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
-julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-isupper.c -o 0-isuper
-julien@ubuntu:~/0x04$ ./0-isuper 
-A: 1
-a: 0
-julien@ubuntu:~/0x04$
