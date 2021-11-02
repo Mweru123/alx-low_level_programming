e4c0cc3559edd9a211fc752b0920588377848e73
wambuimweru@gmail.com <wambuimweru@gmail.com>
2021-11-02 05:29:27 -0700

    add main.h

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _memset - fill n bytes of memory with a constant value
  * @s: pointer to memory area
  * @b: constant value
  * @n: number of bytes to fill
- * Return: memory area
+ * Return: memory location
  */
 
 char *_memset(char *s, char b, unsigned int n)
